0df6b596d57b7966cd8ad0d44785889894d001b3
lnp57 <148878143+lnp57@users.noreply.github.com>
2025-10-28 21:46:04 +0000

    Deploying to gh-pages from @ lnp57/lnp57.github.io@97fc230000feea55eb1c03aebd2a9bde4c4e328e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -105,11 +105,6 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
